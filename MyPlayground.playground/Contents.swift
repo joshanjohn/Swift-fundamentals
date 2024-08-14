cd425040c0425e7294b597d7d7b31dc04b17cf76
Joshan john <joshanjohn2003@gmail.com>
2024-08-14 16:31:12 +0100

    printing and variable

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+//printing a variable data
+var greeting = "Hello, playground"
+print(greeting)
+
+//printing directly
+print("Let's play Swift")
+
+// printing with a variable
+var place = "Dublin"
+print("Book flights to", place)
---
+++ variables.playground/Contents.swift
@@ -0,0 +1,37 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+// Strings
+var name:String = "Drake"
+
+//Characters
+var firstLetter: Character = "D"
+
+// Integers
+var age:Int = 21
+
+// Double (decimal)
+var weight:Double = 88.9
+
+// Float (decimal)
+var shareprice:Float = 3.14
+
+// Boolean (true/false)
+var isPlayer:Bool = true
+
+// Collections
+var integerCollection: [Int] = [1,2,3,45,5]
+var stringCollection: [String] = ["Jack", "Tom", "Anna"]
+var doubleCollection: [Double] = [2.3 ,2.4, 2.5]
+var floatCollection: [Float] = [1.23, 1.32, 3.21]
+
+// 2D Collectiobs
+var string_stringMap: [String:String] = [
+    "student1":"Drake",
+    "student2":"Tom",
+    "stduent3": "Anna"
+]
+
+
+
