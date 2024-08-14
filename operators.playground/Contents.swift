50d52a1646b6ca33dc8e562b33878cb1034d26c9
Joshan john <joshanjohn2003@gmail.com>
2024-08-14 23:31:25 +0100

    operators

---
+++ operators.playground/Contents.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+var a = 2
+var b = 2
+var c = 2
+var d = 4.5
+
+print(
+    a * b * c
+)
+print(a + b)
+print(a - b)
+print(a/b)
+print(a%b)
+ 
+pow(3,2)
+
+sqrt(4)
+
+
+ceil(d)
+
+floor(d)
